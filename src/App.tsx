4fd832fa585d17ebfeeabda804f5989f7057b523
Felipe Moura <86014843+felagmoura@users.noreply.github.com>
2025-10-31 14:22:18 -0300

    feat: Add GeneratorPage component for idea generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { LandingPage } from "@/pages/LandingPage/LandingPage";
+//import { GeneratorPage } from "./pages/GeneratorPage/GeneratorPage";
 
 export default function App() {
   return <LandingPage />;
+  //return <GeneratorPage />;
+
 }
 
